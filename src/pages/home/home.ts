4b64cfd1f06d1c56e985198214368e581614a7e6
Alexandru Florescu <acdc152@gmail.com>
2018-07-23 20:42:00 +0300

    weather app incomplete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WeatherProvider } from '../../providers/weather/weather';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  weather: any;
+  coords: {
+    latitude:any,
+    longitude:any,
+  };
+  city: any;
+
+  constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {
 
   }
 
+  ionViewWillEnter(){
+    this.coords = {
+      latitude: '38.123',
+      longitude: '-78.543'
+    }
+    this.city = 'Bucharest';
+    this.weather = {};
+    // this.weatherProvider.getWeatherByCoords(this.coords.latitude, this.coords.longitude)
+    // .subscribe(weather => {
+    //   this.weather = weather.data[0];
+    //   console.log(this.weather);
+    // });
+    this.weatherProvider.getWeatherByCity(this.city)
+    .subscribe(weather => {
+      this.weather = weather.data[0];
+      console.log(this.weather)
+    });
+    console.log(this.weather);
+  }
 }
---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,37 @@
+import {  HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+/*
+  Generated class for the WeatherProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class WeatherProvider {
+
+  url: any;
+
+  constructor(public http: HttpClient) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'https://api.weatherbit.io/v2.0/current?key=a6e28c2b0a6248fab58b67bcb94dc70e&';
+  }
+
+  getWeatherByCoords(latitude, longitude){
+    // var headers = new HttpHeaders({
+    //   'Access-Control-Allow-Origin' : '*',
+    //   "X-Requested-With": "XMLHttpRequest"
+    // });
+    return this.http
+      .get( this.url + 'lat=' + latitude + '&lon=' + longitude)
+      .map(res=> res);
+  }
+
+  getWeatherByCity(city){
+    return this.http
+      .get( this.url + 'city=' + city)
+      .map(res=> res);
+    }
+
+
+}
